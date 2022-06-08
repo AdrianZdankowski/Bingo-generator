07ab232f37b38451a26ced7e9b60ecfef0f2953d
AdrianZdankowski <adrianzdankowski@gmail.com>
2022-06-08 17:45:16 +0200

    Added board

--- js/script.js
+++ js/script.js
@@ -4,11 +4,30 @@ const cellPreview = document.getElementById("cellPreview")
 const boardOptions = document.querySelectorAll(".boardOption")
 
 const handleConfirmation = () => {
-    const boardSize = document.getElementById("board-size-select").value
+    const boardSize = document.getElementById("board-size-select")
     const boardBgColor = document.getElementById("board-bg-color").value
     const boardCellFontColor = document.getElementById("board-cell-font-color").value
     const boardCellBgColor = document.getElementById("board-cell-bg-color").value
-    document.getElementById("container").style.display = "none"
+    const board = document.getElementById("board") 
+        
+    document.getElementById("container").remove();
+    board.style.setProperty('display', 'grid');
+    board.style.setProperty('grid-template-columns', 'repeat(' + boardSize.value + ', 1fr)');
+    board.style.setProperty('grid-template-rows', 'repeat(' + boardSize.value + ', 1fr)');
+    
+    // Zamienić na zmienne css ^^^^
+
+    for (let i = 1; i <= (boardSize.value ** 2); i++) {
+        const cell = document.createElement("div");
+        const inputField = document.createElement("input")
+        cell.id = i
+        cell.classList.add("cell");
+        inputField.placeholder = "TEST"
+        cell.append(inputField)
+        board.append(cell)
+    }
+
+
 }
 
 const handleSetupClick = event => {
