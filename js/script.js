9b49f608ce460d4260af0f4d73c63115c7c6d13a
AdrianZdankowski <adrianzdankowski@gmail.com>
2022-06-08 22:15:12 +0200

    Refactored a bit

--- js/script.js
+++ js/script.js
@@ -2,33 +2,41 @@ const setupBtn = document.getElementById("setup-btn")
 const preview = document.getElementById("preview")
 const cellPreview = document.getElementById("cellPreview")
 const boardOptions = document.querySelectorAll(".boardOption")
+const root = document.querySelector(':root')
+const boardTitle = document.getElementById("board-title")
+
+
 
 const handleConfirmation = () => {
     const boardSize = document.getElementById("board-size-select")
-    const boardBgColor = document.getElementById("board-bg-color").value
     const boardCellFontColor = document.getElementById("board-cell-font-color").value
     const boardCellBgColor = document.getElementById("board-cell-bg-color").value
     const board = document.getElementById("board") 
         
     document.getElementById("container").remove();
     board.style.setProperty('display', 'grid');
-    board.style.setProperty('grid-template-columns', 'repeat(' + boardSize.value + ', 1fr)');
-    board.style.setProperty('grid-template-rows', 'repeat(' + boardSize.value + ', 1fr)');
+    boardTitle.style.setProperty('display', 'block');
+    root.style.setProperty('--board-size', boardSize.value)
     
     // Zamienić na zmienne css ^^^^
 
     for (let i = 1; i <= (boardSize.value ** 2); i++) {
+
         const cell = document.createElement("div");
-        const inputField = document.createElement("input")
+        const inputField = document.createElement("p")
         cell.id = i
         cell.classList.add("cell");
+        inputField.textContent = "TEST"
+        inputField.setAttribute('contenteditable','true')
+        inputField.style.setProperty('color', boardCellFontColor)
+        cell.style.setProperty('background-color', boardCellBgColor)
         inputField.placeholder = "TEST"
         cell.append(inputField)
         board.append(cell)
+
+        }
     }
 
-
-}
 
 const handleSetupClick = event => {
     const field = event.target
